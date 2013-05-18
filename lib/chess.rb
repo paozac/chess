19a2f812bdfe02430fbc2c6deb66090e651ff45b
Paolo Zaccagnini <paozac@gmail.com>
2013-05-18 12:56:54 +0100

    Add some active_support extensions

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,2 +1,6 @@
 require 'chess/game'
 require 'chess/pgn'
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/string/starts_ends_with'
+
