45cb31d8fed74db5e29d0dbbd671d4a07c2204de
Paolo Zaccagnini <paozac@gmail.com>
2013-05-27 10:45:48 +0100

    Initial board setup

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -3,9 +3,39 @@ module Chess
   # to the `Game` class.
   #
   class Board
+    # Use FEN pieces notation format. Lowercase is black, uppercase white.
+    # I'm using a bottom-top representation for easier human visualization.
+    #
+    INITIAL_SETUP = [
+      [:r,  :n,  :b,  :q,  :k,  :b,  :n,  :r ],
+      [:p,  :p,  :p,  :p,  :p,  :p,  :p,  :p ],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [:P,  :P,  :P,  :P,  :P,  :P,  :P,  :P ],
+      [:R,  :N,  :B,  :Q,  :K,  :B,  :N,  :R ]
+    ]
+
+    # Public: bidimensional array of the board squares
+    #
+    attr_accessor :squares
+
+    def initialize(setup = INITIAL_SETUP)
+      self.squares = setup
+    end
+
     # Public: returns the FEN representation of the current board state. This
     # doesn't include the castling info and such.
     def to_fen
+    end
+
+    # Public: returns a board with the initial setup
+    #
+    def self.default
+      new
     end
   end
 end
--- spec/lib/chess/board_spec.rb
+++ spec/lib/chess/board_spec.rb
@@ -4,4 +4,16 @@ describe Chess::Board do
   describe "#to_fen" do
     pending
   end
+
+  describe ".default" do
+    subject { described_class.default }
+
+    it "returns a Board object" do
+      expect(subject).to be_kind_of(Chess::Board)
+    end
+
+    it "stores the default setup" do
+      expect(subject.squares).to eq(Chess::Board::INITIAL_SETUP)
+    end
+  end
 end
